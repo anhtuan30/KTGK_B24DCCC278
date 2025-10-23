52d84c9f25d2ad42a3de7d07ef326779190a75d4
NguyenDangAnhTuanB24DCCC278 <tuanlorion102@gmail.com>
2025-10-23 09:03:27 +0700

    Update PostList.tsx

--- Components/PostList.tsx
+++ Components/PostList.tsx
@@ -1,4 +1,4 @@
-import { Post } from "../types";
+import { Post } from "../types/Post";
 import PostCard from "./PostCard";
 import { Link } from "react-router-dom";
 import { useState } from "react";
