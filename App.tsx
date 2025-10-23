a8e044cb182d156ff3078f29addde6e42a08287e
NguyenDangAnhTuanB24DCCC278 <tuanlorion102@gmail.com>
2025-10-23 08:20:18 +0700

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,46 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import PostList from "./components/PostList";
+import PostDetail from "./components/PostDetail";
+import PostForm from "./components/PostForm";
+import { Post } from "./types";
+
+function App() {
+  const [posts, setPosts] = useState<Post[]>([
+    {
+      id: "1",
+      title: "React Router cơ bản",
+      author: "Nguyễn Văn A",
+      thumbnail: "https://picsum.photos/400/200?1",
+      content: "Nội dung bài viết mẫu ...",
+      category: "Công nghệ",
+      date: "2025-10-21",
+    },
+    {
+      id: "2",
+      title: "Hành trình phượt Đà Lạt",
+      author: "Trần Văn B",
+      thumbnail: "https://picsum.photos/400/200?2",
+      content: "Nội dung bài viết mẫu ...",
+      category: "Du lịch",
+      date: "2025-10-20",
+    }
+    
+  ]);
+
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<PostList posts={posts} setPosts={setPosts} />} />
+        <Route path="/posts" element={<PostList posts={posts} setPosts={setPosts} />} />
+        <Route path="/posts/:id" element={<PostDetail posts={posts} setPosts={setPosts} />} />
+        <Route path="/create" element={<PostForm posts={posts} setPosts={setPosts} />} />
+        <Route path="/posts/edit/:id" element={<PostForm posts={posts} setPosts={setPosts} />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
